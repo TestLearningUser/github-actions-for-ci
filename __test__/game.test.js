6d9d165f37fa97e9757e3794417f91849118a743
kiloeras <kiloerasss@gmail.com>
2020-08-11 13:11:07 +0200

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,21 +1,11 @@
 const Game = require('../src/game').default
-const fs = require('fs')
-
-describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
-    fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      expect(err).toBe(null)
-      expect(data).toMatchSnapshot()
-      done()
-    })
-  })
-})
 
 describe('Game', () => {
   let game, p1, p2
   beforeEach(() => {
     p1 = 'Salem'
     p2 = 'Nate'
+    this.p2 = p2
     game = new Game(p1, p2)
   })
 
